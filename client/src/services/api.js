d734c30a22f55f2eedbbcb1377da3dd19e61299a
workmdbank-hash <workmdbank@gmail.com>
2026-08-20 14:29:36 +0630

    https://angry-fans-buy.loca.lt

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,5 +1,5 @@
 // This automatically detects if you are on localhost (PC) or an IP address (Phone)!
-const API_URL = 'https://empty-baths-hug.loca.lt/api';
+const API_URL = 'https://angry-fans-buy.loca.lt/api';
 // ==========================================
 // PRODUCT FUNCTIONS
 // ==========================================
